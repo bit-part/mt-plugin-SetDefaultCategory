e636363becb78efda712be644c1dd5993e13d4cd
tinybeans <register@tinybeans.net>
2014-01-31 15:29:01 +0900

    Added an option "Use setting Default Categories after saving Entry"

--- plugins/SetDefaultCategory/lib/SetDefaultCategory/Callbacks.pm
+++ plugins/SetDefaultCategory/lib/SetDefaultCategory/Callbacks.pm
@@ -1,6 +1,7 @@
 package SetDefaultCategory::Callbacks;
 use strict;
 use warnings;
+use MT::Placement;
 
 sub template_param_edit_entry {
     my ($cb, $app, $param, $tmpl) = @_;
@@ -19,6 +20,41 @@ sub template_param_edit_entry {
     if ($length < 1) {
         $param->{selected_category_loop} = \@default_cats;
     }
+    return 1;
+}
+
+sub cms_post_save_entry {
+    my ($cb, $app, $obj, $orig_obj) = @_;
+
+    my $blog_id = $app->blog->id;
+    my $plugin = MT->component('SetDefaultCategory');
+    my $conf_post_save_enable = $plugin->get_config_value('post_save_enable', 'blog:' . $blog_id) or return;
+    return if ($conf_post_save_enable eq 'disable');
+
+    my $conf_default_cats = $plugin->get_config_value('default_cats', 'blog:' . $blog_id) or return;
+    my $entry_id = $obj->id;
+
+    $conf_default_cats =~ s/\s//g;
+    my @default_cats = split(',', $conf_default_cats);
+
+    unless (my $cat = $obj->category) {
+        my $first = 1;
+        foreach my $cat_id (@default_cats) {
+            my $place = MT::Placement->new;
+            $place->blog_id($blog_id);
+            $place->entry_id($entry_id);
+            $place->category_id($cat_id);
+            if ($first) {
+                $place->is_primary(1);
+            }
+            else {
+                $place->is_primary(0);
+            }
+            $place->save;
+            $first = 0;
+        }
+    }
+
     return 1;
 }
 
